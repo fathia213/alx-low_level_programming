d5396640a164dbc4e8975e407b6f00b7830696ff
fathia <fathiashittu19@gmail.com>
2023-06-16 16:11:23 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Reverse numbers'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{	char cha;
+
+	for (cha = 'z'; cha >= 'a'; cha--)
+	{
+	printf("%c", cha);
+	}
+	printf("\n");
+	return (0);
+}
