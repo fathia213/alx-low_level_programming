ea792633a830430303218f4b2df80085dc3d6df1
fathia <fathiashittu19@gmail.com>
2023-06-18 00:10:44 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Description: 'two two-digit'
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	int first, second;
+
+	for (first = 0; first <= 98; first++)
+	{
+		for (second = first + 1; second <= 99; second++)
+		{
+			putchar((first / 10) + '0');
+			putchar((first % 10) + '0');
+			putchar(' ');
+			putchar((second / 10) + '0');
+			putchar((second % 10) + '0');
+
+			if (first == 98 && second == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
