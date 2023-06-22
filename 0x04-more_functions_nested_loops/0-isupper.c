c96f6ef7f52cd892254a2d48e457c4711e1e204b
fathia <fathiashittu19@gmail.com>
2023-06-22 10:37:20 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - case checker
+ * Description: 'Alphabet checker'
+ * @c: character input
+ * Return: 1 or 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+	return (1);
+	}
+	return (0);
+}
