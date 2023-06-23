43e8bccd808d9c51d6e03565b55b7c7e2826cb34
fathia <fathiashittu19@gmail.com>
2023-06-23 07:45:23 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - case checker
+ * Description: 'multiple'
+ * @a: character input
+ * @b: character input
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
