3f71ac251ab245af4f43edc5748f626eb26f2cb6
fathia <fathiashittu19@gmail.com>
2023-06-26 12:16:39 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,145 @@
+#include "main.h"
+/**
+ * _puts - pointers and string
+ * Description: 'prints a string, followed by a new line, to stdout'
+ * @str: String
+ * Return: void
+ */
+void _puts(char *str)
+{	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(*(str + i));
+		i++;
+	}
+	_putchar('\n');
+}#include "main.h"
+/**
+ *  * _puts - pointers and string
+ *   * Description: 'prints a string, followed by a new line, to stdout'
+ *    * @str: String
+ *     * Return: void
+ *      */
+void _puts(char *str)
+{	int i = 0;
+
+		while (str[i] != '\0')
+		{
+					_putchar(*(str + i));
+							i++;
+		}
+			_putchar('\n');
+}#include "main.h"
+/**
+ *  * _puts - pointers and string
+ *   * Description: 'prints a string, followed by a new line, to stdout'
+ *    * @str: String
+ *     * Return: void
+ *      */
+void _puts(char *str)
+{	int i = 0;
+
+		while (str[i] != '\0')
+		{
+					_putchar(*(str + i));
+							i++;
+		}
+			_putchar('\n');
+}#include "main.h"
+/**
+ *  * _puts - pointers and string
+ *   * Description: 'prints a string, followed by a new line, to stdout'
+ *    * @str: String
+ *     * Return: void
+ *      */
+void _puts(char *str)
+{	int i = 0;
+
+		while (str[i] != '\0')
+		{
+					_putchar(*(str + i));
+							i++;
+		}
+			_putchar('\n');
+}#include "main.h"
+/**
+ *  * _puts - pointers and string
+ *   * Description: 'prints a string, followed by a new line, to stdout'
+ *    * @str: String
+ *     * Return: void
+ *      */
+void _puts(char *str)
+{	int i = 0;
+
+		while (str[i] != '\0')
+		{
+					_putchar(*(str + i));
+							i++;
+		}
+			_putchar('\n');
+}#include "main.h"
+/**
+ *  * _puts - pointers and string
+ *   * Description: 'prints a string, followed by a new line, to stdout'
+ *    * @str: String
+ *     * Return: void
+ *      */
+void _puts(char *str)
+{	int i = 0;
+
+		while (str[i] != '\0')
+		{
+					_putchar(*(str + i));
+							i++;
+		}
+			_putchar('\n');
+}#include "main.h"
+/**
+ *  * _puts - pointers and string
+ *   * Description: 'prints a string, followed by a new line, to stdout'
+ *    * @str: String
+ *     * Return: void
+ *      */
+void _puts(char *str)
+{	int i = 0;
+
+		while (str[i] != '\0')
+		{
+					_putchar(*(str + i));
+							i++;
+		}
+			_putchar('\n');
+}#include "main.h"
+/**
+ *  * _puts - pointers and string
+ *   * Description: 'prints a string, followed by a new line, to stdout'
+ *    * @str: String
+ *     * Return: void
+ *      */
+void _puts(char *str)
+{	int i = 0;
+
+		while (str[i] != '\0')
+		{
+					_putchar(*(str + i));
+							i++;
+		}
+			_putchar('\n');
+}#include "main.h"
+/**
+ *  * _puts - pointers and string
+ *   * Description: 'prints a string, followed by a new line, to stdout'
+ *    * @str: String
+ *     * Return: void
+ *      */
+void _puts(char *str)
+{	int i = 0;
+
+		while (str[i] != '\0')
+		{
+					_putchar(*(str + i));
+							i++;
+		}
+			_putchar('\n');
+}
